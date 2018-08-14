fd5fa71a8f03b49eb8fa77e0eb95551eca06b29c
root <pauloafpjunior@hotmail.com>
2018-08-14 10:48:27 -0300

    database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'tulipdb',
+	'hostname' => getenv('HOSTNAME'),	
+	'username' => getenv('USERNAME'),	
+	'password' => getenv('PASSWORD'),
+	'database' => getenv('DATABASE'),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
